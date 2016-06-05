0e0cfde2eda1aca2454dc5bed4b9cd595b256bed
Nelson Cespedes <nelsonc24@gmail.com>
2016-06-05 11:46:03 +1000

    datepicker.pt-VE.js
    The format in Spanish.

---
+++ dist/js/i18n/datepicker.pt-VE.js
@@ -0,0 +1,12 @@
+;(function ($) { $.fn.datepicker.language['es-VE'] = {
+    days: ['Domingo', 'Lunes', 'Martes', 'Miercoles', 'Jueves', 'Viernes', 'Sabado'],
+    daysShort: ['Dom', 'Lun', 'Mar', 'Mie', 'Jue', 'Vie', 'Sat'],
+    daysMin: ['Do', 'Lu', 'Ma', 'Mi', 'Ju', 'Vi', 'Sa'],
+    months: ['January','February','March','April','May','June', 'July','August','September','October','November','December'],
+    monthsShort: ['Ene', 'Feb', 'Mar', 'Abr', 'May', 'Jun', 'Jul', 'Ago', 'Sep', 'Oct', 'Nov', 'Dec'],
+    today: 'Hoy',
+    clear: 'Clear',
+    dateFormat: 'dd/mm/yyyy',
+    timeFormat: 'hh:ii aa',
+    firstDay: 0
+}; })(jQuery);
